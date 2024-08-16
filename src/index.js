eb7df47975e56216220441b6b5b466e95c4faf3e
RaoHai29 <raohai216@gmail.com>
2024-08-16 09:03:35 -0700

    updating utililties for Error Handling Api's and Async Handling

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import connectDB from '../database/index.js'
 import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+import express from 'express'
 
 dotenv.config(
     {
@@ -7,7 +10,27 @@ dotenv.config(
     }
 )
 
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+}))
+
+app.use(express.json({limit: "32kb"}))
+app.use(express.urlencoded({extended:true, limit:"32kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+
+
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running on ${process.env.PORT} `)
+    })
+})
+.catch((error)=>{
+    console.log("ERRORRR in mongo connection!!",error)
+})
 
 
 
